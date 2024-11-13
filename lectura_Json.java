fe71b9db1f64a3a29a7b99cf032b7afb0d88df27
sebmarval <156669132+sebmarval@users.noreply.github.com>
2024-11-12 20:52:53 -0400

    Update lectura_Json.java

---
+++ lectura_Json.java
@@ -0,0 +1,28 @@
+class FamilyMember {
+    String name;
+    String[] bornTo;
+    String[] fatherOf;
+    String known_As;
+    String heldTitle;
+    String wedTo;
+    String eyeColor;
+    String hairColor;
+    String house;
+    String notes;
+    String fate;
+
+    public FamilyMember(String name, String[] bornTo, String[] fatherOf, String known_As, String heldTitle, String wedTo, String eyeColor, String hairColor,  String house, String notes, String fate) {
+        this.name = name;
+        this.bornTo = bornTo;
+        this.fatherOf = fatherOf;
+        this.known_As = known_As;
+        this.wedTo = wedTo;
+        this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
+        this.heldTitle = heldTitle;
+        this.house = house;
+        this.notes = notes;
+        this.fate = fate;
+    }
+}
+

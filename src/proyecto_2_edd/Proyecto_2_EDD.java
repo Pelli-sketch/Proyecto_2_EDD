5d52b94248c042b261eb961465dd149983379af4
Pablo Pelliccioni <73903709+Pelli-sketch@users.noreply.github.com>
2024-11-20 08:31:26 -0400

    Create Proyecto_2_EDD.java

---
+++ src/proyecto_2_edd/Proyecto_2_EDD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_2_edd;
+
+/**
+ *
+ * @author sebma
+ */
+public class Proyecto_2_EDD {
+
+    public static void main(String[] args) {
+        try {
+//            LectorJson.leerJson("Baratheon.json");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.toString());
+        }
+    }
+}
